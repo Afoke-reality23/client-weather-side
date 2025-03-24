e4ce99f4ae8ed58f4da27e470125eb30bb118d1a
Reality <Ighoafokereality1@gmail.com>
2025-03-24 17:09:34 +0100

    sending only message no nomore id

--- app.js
+++ app.js
@@ -24,10 +24,10 @@ document.addEventListener("DOMContentLoaded", () => {
   // const regionState = document.querySelector(".region_state");
   // const cloud = document.querySelector(".cloud");
   // const country = document.querySelector(".country");
-  const clientId =
-    localStorage.getItem("clientId") || Math.random().toString(36).slice(2, 6);
-  console.log(clientId);
-  localStorage.setItem("clientId", clientId);
+  //const clientId =
+    //localStorage.getItem("clientId") || Math.random().toString(36).slice(2, 6);
+  //console.log(clientId);
+  //localStorage.setItem("clientId", clientId);
   btn.addEventListener("click", fetchData);
   btn.click();
 
@@ -56,7 +56,6 @@ document.addEventListener("DOMContentLoaded", () => {
         },
         body: JSON.stringify({
           city: text,
-          clientId: clientId,
         }),
       }
     )
